eca56dc673c823bcbd0e8e46a6945e512501bc1e
ihpolyphe <hayato.8124950@mineo.jp>
2022-05-22 23:19:01 +0900

    first @ush

---
+++ README.md
@@ -0,0 +1,16 @@
+# ローカル MySQL のアクセス方法
+
+service mysql start<
+sudo mysql -u hayato -p<password>
+
+# MySQL のセキュリティポリシーの緩和
+
+https://qiita.com/keisukeYamagishi/items/d897e5c52fe9fd8d9273
+
+# テーブルの操作
+
+https://qiita.com/ryosuketter/items/713c7046314ecdf1a4a9
+
+# カラムの削除、追加
+
+https://www.dbonline.jp/mysql/table/index18.html
---
+++ backend/public/javascripts/db_config.js
@@ -0,0 +1,14 @@
+exports.mysql_setting = {
+  host: "*",
+  user: "dev",
+  password: "*",
+  database: "chat_app_db",
+  timezone: "jst",
+};
+
+exports.loginSQL =
+  "SELECT * from user_infomations where user_id=? AND password=?";
+exports.insertRecordSQL =
+  "INSERT INTO chat_histories(history_index, user_id,message,date_time) VALUES(?,?,?,?)";
+exports.getHistoriesSQL = "SELECT * FROM chat_histories";
+exports.deleteHsitorySQL = "TRUNCATE TABLE chat_histories";
